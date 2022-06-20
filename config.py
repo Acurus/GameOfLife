60190ebe57b5191ab65391ee6d852d1243638f27
Jan Tore <jtkyrdalen@gmail.com>
2022-06-20 22:47:08 +0200

    Adjustet config and added boards

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import sys
 BOARD_FILENAME = "boards/board60x60.txt"
 LOG_FILENAME = "logfile.log"
 
-ROUNDS_TO_SIMULATE = 10  # How many rounds to simulate
+ROUNDS_TO_SIMULATE = 250  # How many rounds to simulate
 
 ALIVE_COLOUR = (0, 255, 0)  # Colour of alive cells in pygame screen
 DEAD_COLOR = (0, 0, 0)  # Colour of dead cells in pygame screen
 SCREEN_SIZE_X = 600  # Pygame screen
 SCREEN_SIZE_Y = 600  # Pygame screen
-TICK = 30  # Pygame clock tick
-LOGGING_LEVEL = "INFO"
+TICK = 60  # Pygame clock tick
+LOGGING_LEVEL = "WARNING"
 DEFAULT_LOGGING = {
     "version": 1,
     "formatters": {
